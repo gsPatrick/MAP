2a38d3ef338b938df7ca66177bb047650f1a7dd9
gsPatrick <patrickgsiqueira@hotmail.com>
2025-07-24 00:47:48 -0300

    filtro de data

--- src/features/Financial/financial.service.js
+++ src/features/Financial/financial.service.js
@@ -1049,6 +1049,6 @@ module.exports = {
   recreateParcelledAccount,
   getMonthlyTrend,
   getExpenseCategorySummary,
-  getIncomeCategorySummary
+  getIncomeCategorySummary,
 getFilteredTransactions
 };
