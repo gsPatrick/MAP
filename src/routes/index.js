e9dc854f1a6521b9ab7bbeae5fc387fced50c1fc
unknown <patrickgsiqueira@hotmail.com>
2025-09-01 04:11:02 -0300

    finalizado54 2foi

--- src/routes/index.js
+++ src/routes/index.js
@@ -70,7 +70,6 @@ mainApiRouter.use('/', adminRoutes);
 mainApiRouter.use('/shared-access', authenticateClientToken, sharedAccessRoutes);
 mainApiRouter.use('/hydration', authenticateClientToken, hydrationRoutes);
 mainApiRouter.use('/affiliate', authenticateClientToken, affiliateRoutes);
-mainApiRouter.use('/mercado-pago', privateMercadoPagoRouter); // Checkout do MP (Privado)
 mainApiRouter.use('/services', authenticateClientToken, serviceRoutes);
 mainApiRouter.use('/availability', authenticateClientToken, availabilityRoutes);
 mainApiRouter.use('/stock', authenticateClientToken, globalStockRouter); // Rota global de estoque
