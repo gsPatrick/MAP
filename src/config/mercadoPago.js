301f9655eedd49ab6e65d40a13a8eae45228ab90
unknown <patrickgsiqueira@hotmail.com>
2025-09-02 05:08:40 -0300

    voltar se dar ruim

--- src/config/mercadoPago.js
+++ src/config/mercadoPago.js
@@ -1,22 +1,7 @@
-// src/config/mercadoPago.js
-require('dotenv').config(); // Garante que as variáveis de ambiente sejam carregadas
-const mercadopago = require('mercadopago');
-const logger = require('../utils/logger');
+const mercadopago = require("mercadopago")
 
-// 1. Valida se a variável de ambiente obrigatória foi carregada
-if (!process.env.MERCADO_PAGO_TOKEN) {
-  throw new Error('A variável de ambiente MERCADO_PAGO_TOKEN não está definida.');
-}
-
-// Log da configuração (sem expor o token completo)
-logger.info('[MP Config] Configurando MercadoPago SDK...');
-
-// 2. Configura a SDK do Mercado Pago com o seu Access Token
 mercadopago.configure({
   access_token: process.env.MERCADO_PAGO_TOKEN,
-});
+})
 
-logger.info('[MP Config] MercadoPago SDK configurado com sucesso.');
-
-// 3. Exporta a instância configurada
-module.exports = mercadopago;
+module.exports = mercadopago
